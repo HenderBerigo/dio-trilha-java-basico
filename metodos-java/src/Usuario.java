5d1852aafa1f67044e4474d7d64fb36748437a84
HenderBerigo <henderson.berigo@gmail.com>
2025-02-03 07:13:21 -0300

    metodos com exemplo smartTV

---
+++ metodos-java/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? : " + smartTv.ligada);
+        smartTv.ligar();
+        smartTv.ligar();
+
+        // System.out.println("Desligando a TV");
+        // smartTv.desligar();
+        // System.out.println("Desligando a TV");
+        // smartTv.desligar();
+        // System.out.println("TV ligada? : " + smartTv.ligada);
+
+        System.out.println("Volume atual? : " + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual? : " + smartTv.canal);
+        smartTv.mudarCanal("mais");
+        smartTv.mudarCanal("mais");
+        smartTv.mudarCanal("menos");
+        smartTv.trocarCanal(13);
+
+    }
+}
